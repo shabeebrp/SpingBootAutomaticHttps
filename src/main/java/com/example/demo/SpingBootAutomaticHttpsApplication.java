6297da72764379ece76ea8386445cc61e7e44404
shabeebrp <shabeebrp@gmail.com>
2019-11-25 13:43:39 +0530

    Inital project setup

---
+++ src/main/java/com/example/demo/SpingBootAutomaticHttpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootAutomaticHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootAutomaticHttpsApplication.class, args);
+	}
+
+}
